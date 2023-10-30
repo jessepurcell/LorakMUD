cd4d0d45845709e48f4d95e4e6628c4ae352c8a3
Jesse Purcell <slaydevil@hotmail.com>
2023-10-30 20:51:32 +1000

    Update scene.py

--- lorak/scene.py
+++ lorak/scene.py
@@ -7,8 +7,14 @@ class BaseScene:
     def __init__(self):
         pass
 
+    def create(self):
+        pass
+
     def update(self):
         pass
 
     def render(self):
+        pass
+
+    def destroy(self):
         pass
