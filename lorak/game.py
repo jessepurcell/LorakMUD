9f571b0136f97428e74a840dff5b25a9883f98c3
Jesse Purcell <slaydevil@hotmail.com>
2023-10-30 20:51:22 +1000

    Update game.py

--- lorak/game.py
+++ lorak/game.py
@@ -2,10 +2,11 @@
 Game class
 Handles general game boilerplate
 """
+from scene import BaseScene
 
 
 class Game:
-    _current_scene = None
+    _current_scene: BaseScene = None
 
     def __init__(self, window_size, window_title, scene):
         self.window_size = window_size
@@ -18,5 +19,8 @@ class Game:
     def render(self):
         pass
 
-    def change_scene(self, scene):
-        pass
+    def change_scene(self, new_scene: BaseScene):
+        if self._current_scene is not None:
+            self._current_scene.cleanup()
+        self._current_scene = new_scene
+        self._current_scene.initialize()
