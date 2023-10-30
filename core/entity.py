7e9dee67a8501caf0ea043ef76b28ad358f0ad47
Jesse Purcell <slaydevil@hotmail.com>
2023-10-30 22:21:24 +1000

    Add entity.py class

--- core/entity.py
+++ core/entity.py
@@ -1,8 +1,12 @@
 """
 Base entity class
 """
+import pygame.sprite
 
 
-class Entity:
+class Entity(pygame.sprite.Sprite):
     def __init__(self):
-        pass
+        super(Entity, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
