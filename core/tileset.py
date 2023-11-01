49475bfba54596555780c582eb40419265a3c2f9
Slaydevil <slaydevil@hotmail.com>
2023-11-01 22:40:06 +1000

    Rewrote tileset.py to better suite needs

--- core/tileset.py
+++ core/tileset.py
@@ -1,1 +1,26 @@
+"""Tileset class"""
+import pygame.image
 
+
+class Tileset:
+    def __init__(self, filepath, tile_size=(32, 32), tile_scale=8):
+        self.filepath = filepath
+        self.tile_size = tile_size
+        self.tile_scale = tile_scale
+        self.image = pygame.image.load(filepath)
+        self.rect = self.image.get_rect()
+        self.tiles = []
+        self.load()
+
+    def load(self):
+        width, height = self.rect.size
+        tile_width, tile_height = self.tile_size
+        for x in range(0, width, tile_width):
+            for y in range(0, height, tile_height):
+                tile = pygame.Surface(self.tile_size)
+                tile.blit(self.image, (0, 0), (x, y, *self.tile_size))
+                tile = pygame.transform.scale(tile, (tile_width * self.tile_scale, tile_height * self.tile_scale))
+                self.tiles.append(tile)
+
+    def __str__(self):
+        return f'{self.__class__.__name__} file:{self.filepath} tile:{self.tile_size}'
--- lorak/test_scene.py
+++ lorak/test_scene.py
@@ -3,12 +3,14 @@ Test scene
 """
 from core.scene import BaseScene
 from core.entity import Entity
+from core.tileset import Tileset
 
 
 class TestScene(BaseScene):
     def __init__(self):
         super().__init__()
-        self._entity_manager.add_entity(Entity())
+        # self._entity_manager.add_entity(Entity())
+        self.tileset = Tileset("assets/textures/16x16_Jerom_CC-BY-SA-3.0.png", (16, 16))
 
     def update(self):
         super().update()
@@ -16,4 +18,7 @@ class TestScene(BaseScene):
 
     def render(self, window):
         super().render(window)
+        window.blit(self.tileset.tiles[6], (0, 0))
+        window.blit(self.tileset.tiles[7], (128, 0))
+        window.blit(self.tileset.tiles[8], (256, 0))
         # print("Test scene rendering")
